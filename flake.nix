6829490de3638d97320e448261d2ca8b3800fa5e
Simon Nyssen <dev@snyssen.be>
2024-07-30 17:59:20 +0200

    feat: [WIP] add basic devcontainer

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          # Replace with any package needed for your project
+          nodejs
+        ];
+      };
+    }
+  );
+}
